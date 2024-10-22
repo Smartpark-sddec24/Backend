2bc238fbc677377e8ffea47511d58e055cc4d82f
William Clemmons <wclemms@iastate.edu>
2024-10-21 22:01:52 -0500

    got sql request working

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const mysql = require('mysql2')
+require('dotenv').config()
+
 const app = express()
 const port = 5000
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB
+})
+
+
 app.get('/', (req, res) => {
     res.send("your stupid")
+})
+
+app.get('/getSpots', (req, res) => {
+    connection.query( "SELECT * FROM Spots", (error, results, fields) => {
+        console.log(results)
+    });
 })
 
 app.listen(port, () => {
